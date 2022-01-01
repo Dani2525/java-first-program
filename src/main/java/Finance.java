829e1e37663dbc6e9b90d5c8cbd5c857c91bf1ed
DANI PHILIP <DaniPhilip5@outlook.com>
2022-01-01 21:25:32 +0000

    mode5

---
+++ src/main/java/Finance.java
@@ -0,0 +1,17 @@
+import java.util.Map; 
+
+public class Finance {
+ public final static String BEST_LOAN_RATES="BestLoanRates";
+ public final static String SAVINGS_CALCULATOR = "savingsCalculator";
+ public final static String MORTGAGE_CALCULATOR ="mortgageCalculator"; 
+
+
+public final static Map<String,String> commandsToUsage= Map.of(
+    BEST_LOAN_RATES, "usage: bestLoanRates", // pastes this to console the key is the name of the command line app
+    SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
+    MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
+
+private static boolean validateCommandArguements(String[] args){
+    
+}
+}
