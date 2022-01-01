c9bd3cead4cd7ef514f6fb3858c1f89f47c43463
DANI PHILIP <DaniPhilip5@outlook.com>
2022-01-01 23:46:24 +0000

    fixed bug

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,7 +1,7 @@
 import java.util.Map; 
 
 public class Finance {
- public final static String BEST_LOAN_RATES="BestLoanRates";
+ public final static String BEST_LOAN_RATES="bestLoanRates";
  public final static String SAVINGS_CALCULATOR = "savingsCalculator";
  public final static String MORTGAGE_CALCULATOR ="mortgageCalculator"; 
 
