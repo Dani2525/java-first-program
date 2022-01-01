a45f59c6c1123f1ba8f45894e19a42dc9f2296f2
DANI PHILIP <DaniPhilip5@outlook.com>
2022-01-01 23:43:16 +0000

    completed the switch statement

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -6,11 +6,20 @@ public class Finance {
  public final static String MORTGAGE_CALCULATOR ="mortgageCalculator"; 
 
 
-public final static Map<String,String> commandsToUsage= Map.of(
+ public final static Map<String,String> commandsToUsage= Map.of(
     BEST_LOAN_RATES, "usage: bestLoanRates", // pastes this to console the key is the name of the command line app
     SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
     MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
-
-    
+ private static boolean validateCommandArguements(String[] args){
+    switch (args[0]) { // setting the args at 0 refers to the name of the app the user wants to use 
+        case BEST_LOAN_RATES:
+            return args.length == 1;// when case is BLR , check if args array length is one and returns the boolean value true or false 
+        case SAVINGS_CALCULATOR:
+            return args.length == 3;// need to in actuality thte dets and credits but add 1 for args[0]
+        case MORTGAGE_CALCULATOR:
+            return args.length == 4;      
+    }
+    return false;
+}
 }
