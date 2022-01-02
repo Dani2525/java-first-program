99a73a3ff49ec6b7d02ce365cd54758eccecd5c4
DANI PHILIP <DaniPhilip5@outlook.com>
2022-01-02 23:57:48 +0000

    for testing

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -1,15 +1,5 @@
 package com.h2;
 
-public class Utilities {
-   public static long getLongValue(String in){
-    Long out= Long.MIN_VALUE;
-    try{
-        Long.parseLong(in)=out;
 
-    }catch (NumberFormatException e){
-
-    }catch (IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program."))
-                return out;
-}
 }
 
