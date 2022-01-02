3f24a9bf683789d6804cca786b97f6c397975593
DANI PHILIP <DaniPhilip5@outlook.com>
2022-01-02 22:26:17 +0000

    module 5 completed

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,4 +1,9 @@
-import java.util.Map; 
+import java.util.Arrays;
+import java.util.Map;
+
+import com.h2.BestLoanRates;
+import com.h2.MortgageCalculator;
+import com.h2.SavingsCalculator; 
 
 public class Finance {
  public final static String BEST_LOAN_RATES="bestLoanRates";
@@ -6,12 +11,12 @@ public class Finance {
  public final static String MORTGAGE_CALCULATOR ="mortgageCalculator"; 
 
 
- public final static Map<String,String> commandsToUsage= Map.of(
+    public final static Map<String,String> commandsToUsage= Map.of(
     BEST_LOAN_RATES, "usage: bestLoanRates", // pastes this to console the key is the name of the command line app
     SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
     MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
- private static boolean validateCommandArguements(String[] args){
+    private static boolean validateCommandArguements(String[] args){
     switch (args[0]) { // setting the args at 0 refers to the name of the app the user wants to use 
         case BEST_LOAN_RATES:
             return args.length == 1;// when case is BLR , check if args array length is one and returns the boolean value true or false 
@@ -22,4 +27,38 @@ public class Finance {
     }
     return false;
 }
+    private static void executeCommand(String command, String[] arguements){// command is the name of t what app the user wants to run and the inputs are the arguemetn s for tyhe app
+    switch (command){ // 
+        case BEST_LOAN_RATES:
+            System.out.println("Finding best loan rates ...");
+            BestLoanRates.main(arguements);// calls the method from bestloanrates which is imported ans arguement sthat user entered are passed 
+            return;
+        case SAVINGS_CALCULATOR:
+            System.out.println("Finding your net savings ...");
+            SavingsCalculator.main(arguements);
+            return;
+        case MORTGAGE_CALCULATOR:
+            System.out.println("Finding your monthly payment ...");
+            MortgageCalculator.main(arguements);
+            return;}
+
+    }
+
+    public static void main(String[] args){
+    String command = args[0]; // command is initializzed to equal to the first arary if the array which is what the user enters 
+    if (!commandsToUsage.containsKey(command)){// if the variavble command does not conatain the commans grom commandto usage map we print
+        System.out.println(command + ": command not found");
+        return;
+    }
+    boolean isValidCommand = validateCommandArguements(args);
+    if (!isValidCommand){//  if not a valod command
+     System.out.println(commandsToUsage.get(args[0])); //  
+     return;
+    }//otherwise
+    executeCommand(command, Arrays.copyOfRange(args, 1, args.length) );// command a sint he app name and copies the parameters bneeded that the user entered into a new arrasy called args
+
+}
+
+
+
 }
